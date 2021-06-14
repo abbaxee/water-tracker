b3d6721b146ae7d5cb7df3fc811e83baf83a3c4a
Abdullahi Aliyu <abbaxee2@gmail.com>
2021-06-14 01:04:17 +0100

    wrote tests for util functions

--- src/components/__tests__/TargetModal.test.js
+++ src/components/__tests__/TargetModal.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Alert, View} from 'react-native';
+import {Alert} from 'react-native';
 import {render, fireEvent} from '@testing-library/react-native';
 import TargetModal from '../TargetModal';
 
@@ -22,18 +22,4 @@ describe('test <TargetModal /> component', () => {
     fireEvent.press(updateButton);
     expect(AlertSpy).toHaveBeenCalled();
   });
-
-  // it('should call the update function if ', () => {
-  //   const {queryByTestId} = render(<TargetModal modalVisible />);
-
-  //   const textInput = queryByTestId('text-input');
-  //   console.log(textInput);
-  //   const updateButton = queryByTestId('update-button');
-  //   const AlertSpy = jest.spyOn(Alert, 'alert');
-
-  //   fireEvent.changeText(textInput, '10');
-
-  //   fireEvent.press(updateButton);
-  //   expect(AlertSpy).not.toHaveBeenCalled();
-  // });
 });
